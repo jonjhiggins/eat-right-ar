dfce012af8357bcc07d2531ab02db5be30b2014b
Jon Higgins <jon@jonhiggins.co.uk>
2023-03-25 17:22:56 +0000

    Remove build files

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QWRKekNybi8vLy8vQUFBQm1ZamdDcmU2eFV5aXZuVy83UDZiTzBNdEd6cEczVEwvODB2d05ETHI4Tm5PNWVNcmVEYVNmcEtRd295UmUwcGxpSUFnaWJobmxKL1pKWlZRNjJQT0JweHFnZnAvd1paUFc4VUkwMnJVUnJwbG53WmFvN0JKeTVOWEZLUC9HYSt0amI2aFJOZUNScE1TUi9oeFJjc2JGVmdJcTNGb0s3d1ZvTVJ6ZnF5Z2FlZ3NjUTlmSHQyOTFGSURvZVpGYmxwVERleHdQZ0lhdjBYeW9XZ1RuMDBCakVJdDVoWldkTGZCUVVkU3I2VHFqRFAyekIwMTNUeWJaK21CdmFBZkNxMTZVV1JjbHVnOXJkd2VvTHRDZ0tsN0FZTVVXa05RWVpLNzZxaFQ1UWR6dlNoWFBvK3ZzVDUzVHF3TnBGTjdyUkZhMU15dnArM1BLckZqR2RoU3NpdXVOa2N3UkFXRTZjaUt0bmhKRVNCL3ZveTY=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
